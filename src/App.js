1478ba63799e2ccaa9a9ecfd2ef0969e9abd6f18
Priyanka Vaidya <vaidyapriyanka80@gmail.com>
2022-02-20 00:21:48 +0530

    Added front end UI files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import Navigation from './components/NavbarContent/Navigation';
+import Home from './components/Home';
+import SearchCabs from './components/SearchCabs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/SearchCabs' element={<SearchCabs />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
